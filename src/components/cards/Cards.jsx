8f97cae1408e4c93363f2e2cf2522eb39f6ad2f2
kishanverma24 <kishanverma9151@gmail.com>
2024-07-06 23:13:45 +0530

    moddified some text files

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -11,9 +11,11 @@ function Cards() {
         <div className="overlay">
           <h4>Toptracer Range </h4>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-            impedit voluptatum delectus mollitia asperiores corporis ab
-            repellendus dicta, amet aperiam?
+            Toptracer Range revolutionizes golf practice with advanced ball
+            tracking technology, providing instant shot feedback and interactive
+            game modes. It enhances driving ranges by attracting golfers of all
+            skill levels with a modern, engaging experience that improves
+            performance and enjoyment.
           </p>
         </div>
       </div>
@@ -25,9 +27,9 @@ function Cards() {
         <div className="overlay">
           <h4>Adventure Golf</h4>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-            impedit voluptatum delectus mollitia asperiores corporis ab
-            repellendus dicta, amet aperiam?
+            Golf Adventures: Discover New Courses, Unforgettable Challenges, and
+            Spectacular Destinations with Golf. Elevate Your Game and Explore
+            the World of Golf in Style.
           </p>
         </div>
       </div>
@@ -39,9 +41,9 @@ function Cards() {
         <div className="overlay">
           <h4>Golf Lessons</h4>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-            impedit voluptatum delectus mollitia asperiores corporis ab
-            repellendus dicta, amet aperiam?
+            Explore New Courses, Master Unforgettable Challenges, and Learn with
+            Expert Lessons from Golf. Elevate Your Game and Discover the Joy of
+            Golfing in Exciting Destinations.
           </p>
         </div>
       </div>
